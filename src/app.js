0faa617c716635896c595470cf0faf7a339bd6e8
brenogcota <brenocota4@gmail.com>
2020-05-06 19:16:49 -0300

    add helmet to security headers

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import routes from './routes';
+import helmet from 'helmet';
 
 import './database';
 
@@ -13,6 +14,7 @@ class App {
 
     middlewares() {
         this.server.use(express.json());
+        this.server.use(helmet());
     }
 
     routes() {
